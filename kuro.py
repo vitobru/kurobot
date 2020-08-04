e1e2c4b8f7693036adee3fce538dc197bca51bd6
Jae <47403253+vitobru@users.noreply.github.com>
2020-08-04 09:34:12 +0100

    Update kuro.py

--- kuro.py
+++ kuro.py
@@ -6,22 +6,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    print(f'{client.user.name} has connected to Discord!')
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hey, {member.name}. Welcome to the server. Sit down and relax or something. The lolis are here.'
     )
-
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
