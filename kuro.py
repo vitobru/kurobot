8caeac54d8847c8acd7870cfe646f8c3e3dd3cc6
Jae <47403253+vitobru@users.noreply.github.com>
2020-08-04 10:17:43 +0100

    Update kuro.py

--- kuro.py
+++ kuro.py
@@ -1,5 +1,6 @@
 #client
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
@@ -19,5 +20,24 @@ async def on_member_join(member):
     await member.dm_channel.send(
         f'Hey, {member.name}. Welcome to the server. Sit down and relax or something. The lolis are here.'
     )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    kuro_quotes = [
+        'Mana supply, pretty please?',
+        (
+            '...What is that look for? '
+            'I get it, everyone else is taking off their clothes and wearing new outfits. '
+            'You were looking forward to it, weren’t you? What a pervert.'
+        ),
+        'I’m a little tired. Mana transfer please, Master',
+    ]
+    
+    if message.content == '!quotes':
+        response = random.choice(kuro_quotes)
+        await message.channel.send(response)
 
 client.run(TOKEN)
