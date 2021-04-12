ba21d2636afd7c15c7c79d54603bf99198267014
Vito <jakobwilczynski211@gmail.com>
2021-04-11 23:58:42 -0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,13 +6,15 @@ TOKEN = ""
 
 # Owner and Dev user ID(s)
 OWNER = 408372847652634624  # vito#1072 as of 04-11-2021
+DEVS = [408372847652634624,261232036625252352]
 
 # Activity settings
 ACTIVITY_NAME = "Prisma Illya"
 ACTIVITY_TYPE = ActivityType.watching
 
 # Extensions to load
-EXTENSIONS = []
+EXTENSIONS = ['cogs.disable',
+              'cogs.music']
 
 # Link to the repo for the !about command.
 REPO_LINK = "https://github.com/vitobru/kurobot"
